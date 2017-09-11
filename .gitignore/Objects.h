d30370ab5f9e34bbf840f988a9cf8ca18a93df5a
IkbenSergi <sergi@live.nl>
2017-09-12 01:34:07 +0200

    Uploaded Assignment 1

---
+++ .gitignore/Objects.h
@@ -0,0 +1,7 @@
+#pragma once
+//Look up your Schedule 
+int[][] LookitUp(string name, string teacher, string class, int classroom)
+{
+	//Search in register for data and return it
+	//return 2D mentional array
+}
