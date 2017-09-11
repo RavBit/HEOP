1dcc9db483e759da8d273e5190b3562a65caaa8e
IkbenSergi <sergi@live.nl>
2017-09-12 01:36:20 +0200

    Delete Objects.h

--- .gitignore/Objects.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//Look up your Schedule 
-int[][] LookitUp(string name, string teacher, string class, int classroom)
-{
-	//Search in register for data and return it
-	//return 2D mentional array
-}
